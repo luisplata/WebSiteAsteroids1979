a2731adb72c0d3cca98e144d6dbf390eb9618293
www.luisplata <www.luisplata@gmail.com>
2026-01-21 21:11:43 +0000

    ++

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import tailwindcss from '@tailwindcss/vite';
 // Determine the base path depending on the environment
 const base = process.env.NODE_ENV === 'production' ? '/WebSiteAsteroids1979' : '/';
 
+console.log(`[DEBUG] NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`[DEBUG] Using base path: ${base}`);
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luisplata.github.io',
